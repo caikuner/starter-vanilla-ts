b61f4477ea8905c81c76fcda3da46f0177e51d3a
caikuner <caikun0229@foxmail.com>
2023-08-13 14:45:18 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 export const one = 1
 export const two = 2
+
+export function sendMessage(message: string, date = new Date(), flash?: boolean): string {
+  return 'OK'
+}
+
+export const config = {
+  name: 'default',
+  price: 12.5,
+  /**
+   * checked state
+   */
+  checked: false,
+  dimensions: {
+    /** width in px */
+    width: 10,
+    /** height in px */
+    height: 10,
+  },
+  tags: (val: Array<string>) => ['common-tag'].concat(val),
+}
